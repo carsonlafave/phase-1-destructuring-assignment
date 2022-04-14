307c7b4149d3316dfe9c674899e93e88eb88032b
carsonlafave <clafaveaz@gmail.com>
2022-04-14 11:52:31 -0700

    commit tree b5a2f0eed90e69733a6f0ebfea91af8aa59647ee

--- index.js
+++ index.js
@@ -46,3 +46,9 @@ const nestedMuppet = {
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+
+const neigh = "horse"
+const moo = "cow"
+const baa = "sheep"
+const oink = "pig"
+const cluck = "chicken"
